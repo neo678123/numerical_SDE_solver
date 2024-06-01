e40a74efca9ae6f0c339f189f267b68cfa6ddf26
nCorneille <n.corneille@student.tue.nl>
2024-06-01 19:02:41 +0200

    Dumb iterative solution

--- iterative_Euler_Maruyama.py
+++ iterative_Euler_Maruyama.py
@@ -6,12 +6,13 @@ import matplotlib.pyplot as plt
 Dumb solution, probably better vectorized way using numpy
 '''
 
-class forward_Euler_Maruyama:
+class iterative_Euler_Maruyama:
     '''
     Framework for solving SDE of the form
         dX = a(t,X)dt + b(t,X)dW
     using the Euler-Maruyama method
     '''
+
     def a(t: float, X: float) -> float: pass
     def b(t: float, X: float) -> float: pass
     t_max: float
@@ -26,15 +27,30 @@ class forward_Euler_Maruyama:
     def nextPoint(self, prevPoint, t):
         '''
         Does 1 iteration of the Euler-Maruyama scheme
-        :param prevPoint: previous data point "X_(i-1)"
         :return: next data point "X_i"
         '''
 
         dW = np.random.normal(0, 1, 1) * math.sqrt(self.dt)
         return prevPoint + self.a(t, prevPoint) * self.dt + self.b(t, prevPoint) * dW
 
+    def iterateSolution(self, X0):
+        '''
+        Solve the SDE using an iterative Euler-Maruyama scheme
+        :param X0:
+        :return: the tuple "(t, X_t)"
+        '''
+        solution = [X0]
+        t = 0
+        n = 0
+        while t < self.t_max:
+            solution.append(*self.nextPoint(solution[-1], t))
+            t += self.dt
+            n += 1
 
-# Press the green button in the gutter to run the script.
+        t_axis = [i * self.dt for i in range(n + 1)]
+        return t_axis, solution
+
+
 if __name__ == '__main__':
     mu = 10
     sigma = 1
@@ -43,27 +59,16 @@ if __name__ == '__main__':
     X0 = 0
 
     # Attempt at solving a path for the Langevin equation
-    langevin = forward_Euler_Maruyama(
+    langevin = iterative_Euler_Maruyama(
         lambda t, X: -mu * X,
         lambda t, X: sigma,
         t_max, dt
     )
 
-    solution = [X0]
-    t = 0
-    n = 0
-    while t < t_max:
-        solution.append(*langevin.nextPoint(solution[-1], t))
-        t += dt
-        n += 1
-
-    t_axis = [i * dt for i in range(n+1)]
+    t_axis, solution = langevin.iterateSolution(X0)
 
     print(t_axis)
     print(solution)
 
     plt.plot(t_axis, solution)
     plt.show()
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
