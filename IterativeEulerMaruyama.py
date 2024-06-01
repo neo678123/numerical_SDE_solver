7ba4bf6a500d693b4763f29d0cdc52b4dd572b31
nCorneille <n.corneille@student.tue.nl>
2024-06-01 20:13:31 +0200

    started on multivariate version

--- IterativeEulerMaruyama.py
+++ IterativeEulerMaruyama.py
@@ -41,6 +41,50 @@ class IterativeEulerMaruyama:
         return solution
 
 
+class IterativeEulerMaruyamaMultivariate:
+    """
+    Framework for solving SDE of the form
+        dX = a(t,X)dt + b(t,X)dW
+    using the Euler-Maruyama method
+    """
+
+    # a(t: float, X: float) -> float
+    # b(t: float, X: float) -> [[float],[float]]
+    # times: [float]
+
+    def __init__(self, a, b, times, dim):
+        """
+        :param a: a(t,X)
+        :param b: b(t,X)
+        :param times: array of time values
+        """
+        self.a = a
+        self.b = b
+        self.times = times
+        self.dim = dim
+
+    def getSolution(self, Xt0):
+        """
+        Solve the SDE using an iterative Euler-Maruyama scheme
+        :param Xt0:
+        :return: the tuple "(t, X_t)"
+        """
+        dt = np.diff(self.times)
+        solution = np.empty([self.dim, len(times)])
+        dW = np.empty([self.dim, len(times)-1])
+
+        # TODO: only works for dim=1, need to do matrix stuff
+        for i in range(self.dim):
+            dW[i] = np.multiply(np.vectorize(math.sqrt)(dt), np.random.normal(0, 1, len(dt)))
+
+            solution[i][0] = Xt0[i]
+            for j in range(len(self.times) - 1):
+                solution[i][j+1] = solution[i][j] + self.a(self.times[j + 1], solution[i][j]) * dt[j] \
+                                    + self.b(self.times[j + 1], solution[i][j]) * dW[i][j]
+
+        return solution
+
+
 if __name__ == '__main__':
     mu = 10
     sigma = 1
@@ -49,13 +93,13 @@ if __name__ == '__main__':
     X0 = 0
 
     # Attempt at solving a path for the Langevin equation
-    langevin = IterativeEulerMaruyama(
+    langevin = IterativeEulerMaruyamaMultivariate(
         lambda t, X: -mu * X,
         lambda t, X: sigma,
-        times
+        times, 1
     )
 
-    solution = langevin.getSolution(X0)
+    solution = langevin.getSolution([X0])
 
-    plt.plot(times, solution)
+    plt.plot(times, *solution)
     plt.show()
